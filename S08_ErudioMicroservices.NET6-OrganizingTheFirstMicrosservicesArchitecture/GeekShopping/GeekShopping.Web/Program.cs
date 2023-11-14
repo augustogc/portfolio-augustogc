ffe8362846144a4d18cc6a7b1f32fcea8efaf613
Augusto <augustogc2@gmail.com>
2023-11-14 16:09:21 -0300

    Atualização com view do ProductUpdate.

--- S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.Web/Program.cs
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.Web/Program.cs
@@ -3,15 +3,15 @@ using GeekShopping.Web.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
 builder.Services.AddHttpClient<IProductService, ProductService>(
     c => c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"])    
 );
 
-var app = builder.Build();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
